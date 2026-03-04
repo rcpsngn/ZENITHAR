5f4421378c9f21f0524f5068d73f612d2ab3b694
rcpsngn <140921004+rcpsngn@users.noreply.github.com>
2026-03-04 14:48:36 +0300

    Implement user registration view

---
+++ backend/accounts/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
